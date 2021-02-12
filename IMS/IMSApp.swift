8d1c63fd55c4b371ae531358cf80098da8691722
wov <wovfeng@gmail.com>
2021-02-12 12:53:39 +0800

    Initial Commit

---
+++ IMS/IMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMSApp.swift
+//  IMS
+//
+//  Created by wov on 2021/2/12.
+//
+
+import SwiftUI
+
+@main
+struct IMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
