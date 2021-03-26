bc5ea5876d7dba1fbc1ece8f8dca74890232c7d4
wov <wovfeng@gmail.com>
2021-03-26 21:32:16 +0800

    add stockout view

--- IMS/Views/GoodDetail.swift
+++ IMS/Views/GoodDetail.swift
@@ -18,78 +18,75 @@ struct GoodDetail: View {
     }
     
     var body: some View {
-            List {
-                Section(header: Text("基本信息")){
-                    HStack{
-                        Text("商品名称")
-                        Spacer()
-                        Text(good.name)
-                    }
-                    HStack{
-                        Text("商品编码")
-                        Spacer()
-                        Text(good.code)
-                    }
-                    HStack{
-                        Text(good.description)
-                    }
-                    
+        List {
+            Section(header: Text("基本信息")){
+                HStack{
+                    Text("商品名称")
+                    Spacer()
+                    Text(good.name)
+                }
+                HStack{
+                    Text("商品编码")
+                    Spacer()
+                    Text(good.code)
+                }
+                HStack{
+                    Text(good.description)
                 }
                 
-
-                Section(header: Text("库存信息")){
-                    HStack{
-                        Text("当前库存")
-                        Spacer()
-                        Text("\(String(good.stock.clean))\(good.unit)")
-                    }
-                    HStack{
-                        Text("已采购库存")
-                        Spacer()
-                        Text("xxx")
-                    }
-                    HStack{
-                        Text("安全库存")
-                        Spacer()
-                        Text("\(good.minimumStock.clean)\(good.unit)")
-                    }
-                    
-                    
-                    Button("出库",action:{
-                        
-                    })
-                    Button("加入到采购单",action:{
-                        
-                    })
+            }
+            
+            
+            Section(header: Text("库存信息")){
+                HStack{
+                    Text("当前库存")
+                    Spacer()
+                    Text("\(String(good.stock.clean))\(good.unit)")
+                }
+                HStack{
+                    Text("已采购库存")
+                    Spacer()
+                    Text("0\(good.unit)")
+                }
+                HStack{
+                    Text("安全库存")
+                    Spacer()
+                    Text("\(good.minimumStock.clean)\(good.unit)")
                 }
                 
+                Button("商品出库",action:{
+                    
+                })
+                Button("加入到采购单",action:{
+                    
+                })
+            }
+            
+            Section(header: Text("库位信息")){
+                HStack{
+                    Text("货架号")
+                    Spacer()
+                    Text(good.shelfNumber)
+                }
+                HStack{
+                    Text("货位号")
+                    Spacer()
+                    Text(good.shelfPosition)
+                }
+            }
+            
+            
+            Section(){
+                Button("删除",action:{
+                    self.delGood(good: good)
+                })
+            }
+            
+        }.navigationTitle(good.name)
+        .listStyle(InsetGroupedListStyle())
         
-                
-                Section(header: Text("库位信息")){
-                    HStack{
-                        Text("货架号")
-                        Spacer()
-                        Text(good.shelfNumber)
-                    }
-                    HStack{
-                        Text("货位号")
-                        Spacer()
-                        Text(good.shelfPosition)
-                    }
-                }
-                
-                
-                Section(){
-                    Button("删除",action:{
-                        self.delGood(good: good)
-                    })
-                }
-                
-            }.navigationTitle(good.name)
-            .listStyle(InsetGroupedListStyle())
-            
-
-            
+        
+        
         
         
     }
---
+++ IMS/Views/StockOut.swift
@@ -0,0 +1,25 @@
+//
+//  StockOut.swift
+//  IMS
+//
+//  Created by wov on 2021/3/26.
+//
+
+import Foundation
+import SwiftUI
+
+//import SwiftUI
+
+struct StockOut: View {
+//    @State private var draftProfile = Profile.default
+
+    var body: some View {
+        Text("Profile for:")
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        StockOut()
+    }
+}
